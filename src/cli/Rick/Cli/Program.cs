a6a72c4dd29082a745b9c7d6a17a84f12d74ab0f
rick <rick@rickdot.net>
2025-05-05 14:46:17 -0500

    yolo a laptop install

--- src/cli/Rick/Cli/Program.cs
+++ src/cli/Rick/Cli/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Cli;
+using SimpleExec;
 
 // will launch into the CLI using various flags
 // there are a few options, looking at Terminal.Gui
@@ -9,8 +10,23 @@ using Cli;
 // only some of them will run the host, and at that,
 // we'll wrap it and make sure we're smart about how
 // hosts are created
-
 Console.WriteLine("Hello World!");
 
+Console.WriteLine("Waiting for 5 seconds...");
+await Task.Delay(5000);
+
+await Command.RunAsync("pacman", "-S --needed base-devel --noconfirm");
+
+await Command.RunAsync("pacman", "-S libnotify hyprpicker wl-clipboard grim slurp rofi-wayland noto-fonts-emoji ttf-jetbrains-mono-nerd --noconfirm");
+
+await Command.RunAsync("paru", "-S ghostty --noconfirm");
+
+await Command.RunAsync("pacman", "-S yazi ffmpeg p7zip jq poppler fd ripgrep fzf zoxide imagemagick --noconfirm");
+
+await Command.RunAsync("pacman", "-S dotnet-sdk --noconfirm");
+
+await Command.RunAsync("pacman", "-S gnome-keyring libsecret --noconfirm");
+
+await Command.RunAsync("pacman", "-S discord flatpak --noconfirm");
 // var host = Host.CreateApplicationBuilder(args).ConfigureHost();
 // host.Run();
